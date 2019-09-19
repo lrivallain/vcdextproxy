c325443801d462edd51c03e17a60d41651ae31f0
Ludovic RIVALLAIN <lrivallain@sii.fr>
2019-09-19 13:01:59 +0000

    still in progress

--- vcdextproxy/rest_worker.py
+++ vcdextproxy/rest_worker.py
@@ -43,14 +43,11 @@ class RESTWorker(Thread):
             message (str): Message to log
         """
         _message = f"[{self.extension_name}] {message}"
-        if level == 'trivia':
-            logger.trivia(_message, args, kwargs)
-        else:
-            try:
-                getattr(logger, level)(_message, args, kwargs)
-            except AttributeError as e:
-                logger.error("Invalid log level {level} used: please fix in code.")
-                self.log("debug", message, *args, **kwargs) # loop with a sure status
+        try:
+            getattr(logger, level)(_message, args, kwargs)
+        except AttributeError as e:
+            logger.error("Invalid log level {level} used: please fix in code.")
+            self.log("debug", message, *args, **kwargs) # loop with a sure status
 
     def get_extension_auth(self):
         """Get the auth object if requested by extension.
@@ -75,7 +72,7 @@ class RESTWorker(Thread):
         # parse information from vcd request metadata. Add them to request headers #10
         headers['org_id'] = self.vcd_data.get('org', '').split("urn:vcloud:org:")[1]
         headers['user_id'] = self.vcd_data.get('user', '').split("urn:vcloud:user:")[1]
-        self.log(trivia, "Headers (without rights): " + json.dumps, headers, indent=2)
+        self.log('trivia', "Headers (without rights): " + json.dumps, headers, indent=2)
         headers['user_rights'] = json.dumps(self.vcd_data.get('rights'))
         return headers
 
@@ -94,7 +91,7 @@ class RESTWorker(Thread):
         if conf(f"{self.conf_path}.uri_replace", False):
             pattern = conf(f"{self.conf_path}.uri_replace.pattern", "")
             by = conf(f"{self.conf_path}.uri_replace.by", "")
-            self.log(debug, f"URI replacement: {pattern} >> {by}")
+            self.log('debug', f"URI replacement: {pattern} >> {by}")
             full_req_path = full_req_path.replace(pattern, by)
         return full_req_path
 
@@ -112,7 +109,7 @@ class RESTWorker(Thread):
             status_code (int): HTTP response code
         """
         # prepare reply properties
-        self.log(info, f"Replying with HTTP response code: {status_code}")
+        self.log('info', f"Replying with HTTP response code: {status_code}")
         resp_prop = {
             "id": self.id,
             "accept": self.headers.get('Accept', None),
@@ -140,12 +137,12 @@ class RESTWorker(Thread):
                 self.req_data.get('method', 'get').lower()
             )
         except AttributeError as e:
-            self.log(error, f"The method {method} is not supported.")
+            self.log('error', f"The method {method} is not supported.")
             rsp_body = {"Error": f"The method {method} is not supported."}
             status_code = 405
             self.reply(rsp_body, status_code)
         except Exception as e:
-            self.log(error, f"Unmanaged error raised: {str(e)}")
+            self.log('error', f"Unmanaged error raised: {str(e)}")
             raise e # raise other errors as usual
         # forward the requests to the backend
         try:
@@ -160,23 +157,23 @@ class RESTWorker(Thread):
             rsp_body = r.text
             status_code = r.status_code
         except requests.exceptions.Timeout:
-            self.log(warning, "Timeout from extension backend server")
+            self.log('warning', "Timeout from extension backend server")
             rsp_body = {"Error": "Timeout from extension backend server"}
             status_code = 504
         except requests.exceptions.TooManyRedirects:
-            self.log(warning, "TooManyRedirects from extension backend server")
+            self.log('warning', "TooManyRedirects from extension backend server")
             rsp_body = {"Error": "TooManyRedirects from extension backend server"}
             status_code = 508
         except requests.exceptions.ConnectionError as e:
-            self.log(warning, "ConnectionError from the extension backend server")
+            self.log('warning', "ConnectionError from the extension backend server")
             rsp_body = {"Error": "ConnectionError from the extension backend server"}
             status_code = 503
         except requests.exceptions.RequestException as e:
-            self.log(warning, "RequestException from extension backend server")
+            self.log('warning', "RequestException from extension backend server")
             rsp_body = {"Error": "RequestException from extension backend server"}
             status_code = 502
         except Exception as e:
-            self.log(error(f"Unmanaged error raised: {str(e)}", exc_info=1))
+            self.log('error'(f"Unmanaged error raised: {str(e)}", exc_info=1))
             rsp_body = {"Error": "Unmanaged error raised"}
             status_code = 500
         self.reply(rsp_body, status_code)
