3db9cf6d612f428d67dfa78879f0b3f4ec371723
Ludovic RIVALLAIN <lrivallain@sii.fr>
2019-09-19 14:55:22 +0000

    still in progress

--- fake_vcd_client/__main__.py
+++ fake_vcd_client/__main__.py
@@ -105,7 +105,7 @@ class VcdSession():
 def main(host, username, password, no_verify):
     """Execute the client.
     """
-    vcd_sess = VcdSession(host, username password,
+    vcd_sess = VcdSession(host, username, password,
         api_version="31.0", verify_ssl=not no_verify)
     logger.info("List current orgs for the user")
     for org in vcd_sess.get('/api/org').get("org", []):
