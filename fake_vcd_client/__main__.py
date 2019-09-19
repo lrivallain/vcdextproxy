c2ed7e2e8cd17a27b8a9d92ee727aab023407da6
Ludovic RIVALLAIN <lrivallain@sii.fr>
2019-09-19 16:14:19 +0000

    still in progress

--- fake_vcd_client/__main__.py
+++ fake_vcd_client/__main__.py
@@ -4,6 +4,7 @@ import json
 import logging
 import click
 import requests
+from time import sleep
 
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -78,6 +79,58 @@ class VcdSession():
         else:
             return r.content
 
+    def put(self, uri_path, data, content_type="application/json", parse_out=True):
+        """Manage PUT requests.
+
+        Args:
+            uri_path (str): path for the REST request.
+            data (str): data to set as request body.
+            content_type (Optional:str): a content type for the request.
+            parse_out (bool): does the output need to be parse as json?
+
+        Returns:
+            dict or str: content of the response body (as interpreted json if possible).
+        """
+        logger.info(f"New PUT request to VCD API: {uri_path}")
+        self.session.headers["Content-Type"] = content_type
+        if content_type == "application/json":
+            data = json.dumps(data)
+        r = self.session.put(
+            f"https://{self.hostname}{uri_path}",
+            verify=self.verify_ssl,
+            data=data
+        )
+        if parse_out:
+            return json.loads(r.content)
+        else:
+            return r.content
+
+    def delete(self, uri_path, data, content_type="application/json", parse_out=True):
+        """Manage DELETE requests.
+
+        Args:
+            uri_path (str): path for the REST request.
+            data (str): data to set as request body.
+            content_type (Optional:str): a content type for the request.
+            parse_out (bool): does the output need to be parse as json?
+
+        Returns:
+            dict or str: content of the response body (as interpreted json if possible).
+        """
+        logger.info(f"New DELETE request to VCD API: {uri_path}")
+        self.session.headers["Content-Type"] = content_type
+        if content_type == "application/json":
+            data = json.dumps(data)
+        r = self.session.delete(
+            f"https://{self.hostname}{uri_path}",
+            verify=self.verify_ssl,
+            data=data
+        )
+        if parse_out:
+            return json.loads(r.content)
+        else:
+            return r.content
+
     def get_auth_token(self, username, password):
         """Retrieve an auth token to authenticate user for further requests.
 
@@ -105,15 +158,42 @@ class VcdSession():
 def main(host, username, password, no_verify):
     """Execute the client.
     """
+    hello_world = {"hello": "world"}
     vcd_sess = VcdSession(host, username, password,
         api_version="31.0", verify_ssl=not no_verify)
     logger.info("List current orgs for the user")
     for org in vcd_sess.get('/api/org').get("org", []):
         print(json.dumps(org, indent=2))
-    logger.info("Requesting data from example1")
-    print(json.dumps(vcd_sess.get('/api/example1/test/toto')))
-    logger.info("Requesting data from example2")
-    print(json.dumps(vcd_sess.get('/api/this/is/1/test/example2/test/azerty?toto')))
+    while True:
+        logger.info("Requesting data from example1")
+        print(json.dumps(
+            vcd_sess.get('/api/example1/test/toto'), indent=2
+        ))
+        sleep(2)
+        logger.info("Requesting data from example2")
+        print(json.dumps(
+            vcd_sess.get('/api/this/is/1/test/example2/test/azerty?toto'),
+            indent=2
+        ))
+        sleep(2)
+        logger.info("Posting data to example1")
+        print(json.dumps(
+            vcd_sess.post('/api/example1/test/toto', hello_world),
+            indent=2
+        ))
+        sleep(2)
+        logger.info("Deleting data in example1")
+        print(json.dumps(
+            vcd_sess.delete('/api/example1/test/toto', {}),
+            indent=2
+        ))
+        sleep(2)
+        logger.info("Updating data in example2")
+        print(json.dumps(
+            vcd_sess.delete('/api/this/is/1/test/example2/test/azerty', {}),
+            indent=2
+        ))
+        sleep(2)
 
 
 if __name__ == '__main__':
